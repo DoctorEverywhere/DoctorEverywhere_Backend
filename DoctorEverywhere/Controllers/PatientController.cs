cf670784179634884c40f052e1de3fd298faf2ba
Marilena Kosma <150340273+marilenakosma@users.noreply.github.com>
2026-05-06 16:32:49 +0300

    Added controllers folder

---
+++ DoctorEverywhere/Controllers/PatientController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoctorEverywhere.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+    }
+}
