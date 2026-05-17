a86c65b8d2ffe0d12e49a79e83580ffc5a232a26
Marilena Kosma <150340273+marilenakosma@users.noreply.github.com>
2026-05-17 16:24:54 +0300

    Added initial Analytics infrastructure and ManagerDto

---
+++ DoctorEverywhere/Services/AnalyticsService.cs
@@ -0,0 +1,31 @@
+﻿using DoctorEverywhere.Domain;
+using DoctorEverywhere.DTOs;
+using DoctorEverywhere.Enums;
+using DoctorEverywhere.Exceptions;
+using DoctorEverywhere.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+namespace DoctorEverywhere.Services
+{
+    public class AnalyticsService : IAnalyticsService
+    {
+        private ApplicationDbContext _context;
+
+        public AnalyticsService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<AppointmentStatusCountDto>> GetAppointmentStatusStats()
+        {
+            return await _context.Appointments
+                .AsNoTracking()
+                .GroupBy(a => a.StatusId)
+                .Select(global => new AppointmentStatusCountDto
+                {
+                    Status = global.Key,
+                    Count = global.Count()
+                })
+                .ToListAsync();
+        }
+    }
+}
---
+++ DoctorEverywhere/Services/Interfaces/IAnalyticsService.cs
@@ -0,0 +1,10 @@
+﻿using DoctorEverywhere.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoctorEverywhere.Services.Interfaces
+{
+    public interface IAnalyticsService
+    {
+        public Task<List<AppointmentStatusCountDto>> GetAppointmentStatusStats();
+    }
+}
